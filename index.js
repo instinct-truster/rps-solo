245e35f30dd76b259aaf0cbdad108eb5f5d36983
Lance Turbes <instinct_truster@protonmail.com>
2023-06-06 18:43:59 -0700

    feat: create rock paper scissor game

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("Hello Friends!");
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice === 0) {
+    return "rock";
+  } else if (computerChoice === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getPlayerChoice() {
+  const playerChoice = prompt(
+    "Please type out your choice of either rock, paper or scissors."
+  );
+  if (
+    playerChoice !== "rock" &&
+    playerChoice !== "paper" &&
+    playerChoice !== "scissors"
+  ) {
+    getPlayerChoice();
+  } else {
+    return playerChoice;
+  }
+}
+
+function getResult(computerChoice, playerChoice) {
+  if (computerChoice === playerChoice) {
+    return "You Tied!";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "You Win!";
+  } else {
+    return "You Lose!";
+  }
+}
+
+function playGame() {
+  const computerChoice = getComputerChoice();
+  const playerChoice = getPlayerChoice();
+  const result = getResult(computerChoice, playerChoice);
+  alert(result);
+}
+
+playGame();
